242d469353d448d2983e3d3bae91630da1e16dbd
Murilo <97199058+MuriloMR@users.noreply.github.com>
2023-01-13 19:26:14 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,56 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum  class  Nivel { BASICO , INTERMEDIARIO , AVAN Ç ADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+classe de dados  Aluno ( val  nome :  String , val  email :  String )
 
-class Usuario
+data class  ConteudoEducacional ( val  nome :  String , val  duracao :  Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class  Formacao ( val  nome :  String , val  conteudos :  List < ConteudoEducacional >, val  nivel :  Nivel ) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+    valinscritos = mutableListOf < Aluno >()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun  matricular ( aluno :  Aluno ) {
+         inscritos.add(aluno)
+    }
+    
+    fun  imprimirListaInscritos (){
+        for ( usuário inscrito){
+            println (usuario.nome)
+        }
+    }
+    
+    fun  imprimirGradeFormacao (){
+        for (cont in conteudos){
+            println ( " ${cont.nome} - ${cont.duracao} " )
+        }
     }
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+diversão  principal () {
+    // cursos individuais
+    val moduloPython =  ConteudoEducacional ( " curso python " , 160 )
+    val moduloKotlin =  ConteudoEducacional ( " curso kotlin " , 60 )
+    
+    val listaConteudos = mutableListOf < ConteudoEducacional >()
+    listConteudos.add(moduloPython)
+    listConteudos.add(moduloKotlin)
+    
+    // formação completa com todos os cursos
+    val formacaoFullstack =  Formação ( " Formação Fullstack Developer " , listConteudos, Nivel . INTERMEDIARIO )
+    
+    // Criar lista de usuários
+    val  André =  Aluno ( " André " , " dede@gmail.com " ) _
+    val  Bruna  =  Aluno ( " Bruna " , " bruna@gmail.com " )
+    
+    // matriculando usuários na formação
+    formacaoFullstack.matricular( André é)
+    formacaoFullstack.matricular( Bruna )
+    
+    println ( " =========================================== " )
+    println ( " Dados da formação " )
+    println ( " ${formacaoFullstack.nome} - ${formacaoFullstack.nivel} " )
+    println ( " Grau de formação " )
+    println (formacaoFullstack.imprimirGradeFormacao())
+    println ( " ========================================== " )
+    println ( " Lista de inscritos " )
+    println (formacaoFullstack.imprimirListaInscritos())
 }
